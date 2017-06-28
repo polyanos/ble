ec09dc9503bd768d4545ce2731f82efd55f3d6c5
polyanos <gregorhoogstad@gmail.com>
2017-06-28 17:35:49 +0200

    Changed the process period

--- BeaconScanner.py
+++ BeaconScanner.py
@@ -26,7 +26,7 @@ class BeaconScanner():
             self.rounds += 1
             print "Beginning round " + str(self.rounds)
             self.scanner.start()
-            self.scanner.process(0.9)
+            self.scanner.process(5)
             self.scanner.stop()
             print "Ending round " + str(self.rounds)
 
