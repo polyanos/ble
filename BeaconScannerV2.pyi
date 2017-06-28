334859e2e54344404e4e8d432193de3555ef1984
polyanos <gregorhoogstad@gmail.com>
2017-06-29 01:42:34 +0200

    Added manufacturer filter

--- BeaconScannerV2.pyi
+++ BeaconScannerV2.pyi
@@ -7,7 +7,20 @@ class BeaconScanner():
 
     def scan_for_time(self, timespan):
         sock = blescan.hci_open_dev(self.hci_number)
-        blescan.parse_events(sock, timespan)
+        result = blescan.start_scan(sock, timespan, beacon_filter)
+
+        for k, v in result.items():
+            print v[0].uuid
+
+        return False
+
+
+
+def beacon_filter(beacon):
+    if beacon._manf == "cdab0215":
+        return True
+    else:
+        return False
 
 scanner = BeaconScanner(0)
 scanner.scan_for_time(1000)
