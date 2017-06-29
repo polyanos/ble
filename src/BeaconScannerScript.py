3c467751b8e3340198675c01de376f195deb158a
polyanos <gregorhoogstad@gmail.com>
2017-06-29 11:19:16 +0200

    Added missing conversions

--- src/BeaconScannerScript.py
+++ src/BeaconScannerScript.py
@@ -10,7 +10,7 @@ round_number = 0
 
 while True:
     round_number += 1
-    print "Starting round " + round_number
+    print "Starting round " + str(round_number)
     scanner.scan_for_timespan(scan_time_span)
-    print "Ending round "  + round_number
+    print "Ending round "  + str(round_number)
     print ""
