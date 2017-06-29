d55699da07139db32d4270c269c1d1f4d4fbab47
polyanos <gregorhoogstad@gmail.com>
2017-06-29 12:50:29 +0200

    Added some debug message

--- src/BeaconScannerScript.py
+++ src/BeaconScannerScript.py
@@ -7,10 +7,15 @@ hci_port_number = 0
 scan_time_span = 1000
 scanner = BeaconScanner(hci_port_number)
 round_number = 0
+result = []
 
 while True:
     round_number += 1
     print "Starting round " + str(round_number)
-    scanner.scan_for_timespan(scan_time_span)
+    result = scanner.scan_for_timespan(scan_time_span)
     print "Ending round "  + str(round_number)
+
+    for item in result:
+        print "Average rssi of " + item.uuid + " is " + str(item.rssi) + "dbm"
+
     print ""
