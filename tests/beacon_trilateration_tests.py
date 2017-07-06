d543a6f17e6ac7b8ba4e1c2e992696c6f0e0615e
Polyanos <gregorhoogstad@gmail.com>
2017-07-06 13:31:01 +0200

    changed value in position algorithm

--- tests/beacon_trilateration_tests.py
+++ tests/beacon_trilateration_tests.py
@@ -31,23 +31,24 @@ class TrilaterationTests(unittest.TestCase):
     def testTrilaterationTestEnoughBeacons(self):
         # Arrange
         beacon1 = _bl.BeaconLocation("1d7837fe978548daa9fa5ffe732b6f51", 66.0, 180.0)
-        beacon2 = _bl.BeaconLocation("5f6962825819435aba0dc2d1fe87deaa", 187.0, 90.0)
+        beacon2 = _bl.BeaconLocation("5f6962825819435aba0dc2d1fe87deaa", 350.0, 90.0)
         beacon3 = _bl.BeaconLocation("6d7b5ac1a4f245218799eb7afd4a30bd", 200.0, 348.0)
         beacon_locations = {beacon1.uuid: beacon1,
                             beacon2.uuid: beacon2,
                             beacon3.uuid: beacon3}
 
         beacon_meta_1 = _bmd.BeaconMetaData("1d7837fe978548daa9fa5ffe732b6f51")
-        beacon_meta_1.estimated_distance = 258.0
+        beacon_meta_1.estimated_distance = -61.0
         beacon_meta_2 = _bmd.BeaconMetaData("5f6962825819435aba0dc2d1fe87deaa")
-        beacon_meta_2.estimated_distance = 241.0
+        beacon_meta_2.estimated_distance = -55.0
         beacon_meta_3 = _bmd.BeaconMetaData("6d7b5ac1a4f245218799eb7afd4a30bd")
-        beacon_meta_3.estimated_distance = 582.0
+        beacon_meta_3.estimated_distance = -65.0
         used_beacons = [beacon_meta_1, beacon_meta_2, beacon_meta_3]
 
         # Act
         distance = trilateration.calculate_position(beacon_locations, used_beacons)
 
         # Assert
+        print distance[0], distance[1]
         self.assertEqual(distance[0], 300, "found value = " + str(distance[0]))
         self.assertEqual(distance[1], 351, "found value = " + str(distance[1]))
